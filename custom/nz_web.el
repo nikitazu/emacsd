b8783070a87dbc43e9fd604863a7deec86d93e3c
Nikita Zuev <nikitazu@gmail.com>
2015-05-03 01:37:54 +0300

    * custom/nz_web.el: css and scss file syntax

--- custom/nz_web.el
+++ custom/nz_web.el
@@ -8,6 +8,7 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.s?css\\'" . web-mode))
 
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
