4e4bdc06bce137ab05e79313fe2b44dca57cbcfd
Nikita Zuev <nikitazu@gmail.com>
2015-05-08 01:38:17 +0300

    Send sql command to get table description

--- custom/nz_sql.el
+++ custom/nz_sql.el
@@ -11,3 +11,12 @@
 			      "--force"
 			      "--verbose"
 			      "--default-character-set=utf8"))
+
+
+
+(defun nz-mysql-describe-table (table)
+  "Send sql command to get table description"
+  (interactive "MTable name:")
+  (let ((query (format "show create table `%s`;"
+		       table)))
+    (sql-send-string query)))
