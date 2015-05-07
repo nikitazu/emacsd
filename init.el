62c279206d0725d26d81535a63a9bb36080d11d1
unknown <nikita.zuev@zuevn08e01.arcadia.intranet>
2015-05-07 10:12:58 +0300

    Cask autochange

--- init.el
+++ init.el
@@ -80,10 +80,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(coffee-tab-width 2)
- '(package-selected-packages
-   (quote
-    (racket-mode coffee-mode highlight-indentation smartparens auto-complete enh-ruby-mode web-mode projectile pallet neotree magit flx-ido ag))))
+ '(package-selected-packages (quote (package-build shut-up epl git commander f dash s))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
