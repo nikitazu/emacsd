1717bff50dc2833764f47dd2e8514166406d7da3
Nikita Zuev <nikitazu@gmail.com>
2015-05-09 14:52:38 +0300

    Racket mode - insert lambda with C-\

--- custom/nz_racket.el
+++ custom/nz_racket.el
@@ -6,7 +6,8 @@
 
 (add-hook 'racket-mode-hook
 	  (lambda ()
-	    (define-key racket-mode-map (kbd "C-c C-c") 'racket-run)))
+	    (define-key racket-mode-map (kbd "C-c C-c") 'racket-run)
+	    (define-key racket-mode-map (kbd "C-\\") 'racket-insert-lambda)))
 
 (add-hook 'racket-mode-hook      #'racket-unicode-input-method-enable)
 (add-hook 'racket-repl-mode-hook #'racket-unicode-input-method-enable)
